e8634a21e1d607fa7f0335cea92965e44e0d616c
jinil <wlsdlfzg@naver.com>
2026-05-12 10:24:26 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.09 ms, Memory: 60.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        boolean isFirst = true;
+        
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                answer.append(c);
+                isFirst = true;
+            } else {
+                if (isFirst) {
+                    answer.append(Character.toUpperCase(c));
+                    isFirst = false;
+                } else {
+                    answer.append(Character.toLowerCase(c));
+                }
+            }
+        }
+        
+        return answer.toString();
+    }
+}
