c7850c2b3ff6b17a95ca100ebe3432fa8c6ff274
jinil <wlsdlfzg@naver.com>
2026-05-13 10:40:16 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 66 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] arr) {
+        int answer = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            answer = ((answer * arr[i]) / gcd(answer, arr[i]));
+        }
+        
+        return answer;
+    }
+    
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        
+        return gcd(b, a % b);
+    }
+}
